b32c3b3951fd59376bf285674f6667a3ba60e8d3
Hung-Alex <alexanderpham59@gmai.com>
2024-06-26 00:14:17 +0700

    Tag Feature

---
+++ src/backend/Application/Features/Tags/Commands/UpdateTag/UpdateTagCommand.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.Responses.Tags;
+using Domain.Behavior;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Features.Tags.Commands.UpdateTag
+{
+    public record UpdateTagCommand(Guid Id, string Name, string Description, string UrlSlug) : IRequest<Result<TagDTO>>, IValidatableRequest;
+}
---
+++ src/backend/Application/Features/Tags/Queries/GetById/GetTagByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.DTOs.Responses.Tags;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Features.Tags.Queries.GetById
+{
+    public record GetTagByIdQuery(Guid Id) : IRequest<Result<TagDTO>>;
+}
