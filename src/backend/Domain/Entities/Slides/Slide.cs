c96b8ccdb29b7e1fcf20a6f446f19674165e222d
Hung-Alex <alexanderpham59@gmai.com>
2024-06-26 18:59:35 +0700

    remove field in Slide table

--- src/backend/Domain/Entities/Slides/Slide.cs
+++ src/backend/Domain/Entities/Slides/Slide.cs
@@ -5,20 +5,18 @@ namespace Domain.Entities.Slides
 {
     public class Slide : BaseEntity, IDatedModification, IAggregateRoot
     {
-        public Slide(string title, string image, string description, bool? status)
+        public Slide(string title, string description, bool? status)
         {
             Title = title ?? throw new ArgumentNullException(); ;
-            Image = image ?? throw new ArgumentNullException();
             Description = description ?? throw new ArgumentNullException();
             Status = status ?? throw new ArgumentNullException();
         }
 
         private Slide() : base() { }
         public string Title { get; set; }
-        public string Image { get; set; }
         public string Description { get; set; }
-        public bool? Status { get; set; } //hide or show banner at homepage
-        public ICollection<SlidesImage> SlidesImages { get; set; }
+        public bool? Status { get; set; }
+        public virtual ICollection<SlidesImage> SlidesImages { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
