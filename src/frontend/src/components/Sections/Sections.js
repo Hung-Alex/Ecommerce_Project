955d5cefa0a6648cf4406cf78a3ab9aa0eee4974
Hồ Trương Huệ Nhật <126588311+hotruonghuenhat@users.noreply.github.com>
2024-06-28 16:00:36 +0700

    add CollectionSlider

--- src/frontend/src/components/Sections/Sections.js
+++ src/frontend/src/components/Sections/Sections.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import CategoryBar from '../CategoryBar/CategoryBar'
 import Banner from '../Banner/Banner'
+import CollectionSlider from '../CollectionSlider/CollectionSlider'
 
 const Sections = () => {
   return (
     <div className='container'>
-        <div className='row'>
-      <CategoryBar />
-
-        </div>
+      <div className='row'>
+        <CategoryBar />
+        <CollectionSlider />
+      </div>
     </div>
   )
 }
