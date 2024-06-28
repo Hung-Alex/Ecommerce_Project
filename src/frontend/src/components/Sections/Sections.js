fdc43a0a7606e5b23692f9859ec28ab428de7862
Hồ Trương Huệ Nhật <126588311+hotruonghuenhat@users.noreply.github.com>
2024-06-28 16:22:52 +0700

    add AdvertisingBanner

--- src/frontend/src/components/Sections/Sections.js
+++ src/frontend/src/components/Sections/Sections.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CategoryBar from '../CategoryBar/CategoryBar'
 import CollectionSlider from '../CollectionSlider/CollectionSlider'
 import SaleBanner from '../SaleBanner/SaleBanner'
+import AdvertisingBanner from '../AdvertisingBanner/AdvertisingBanner'
 
 const Sections = () => {
   return (
@@ -10,6 +11,7 @@ const Sections = () => {
         <CategoryBar />
         <CollectionSlider />
         <SaleBanner />
+        <AdvertisingBanner />
       </div>
     </div>
   )
