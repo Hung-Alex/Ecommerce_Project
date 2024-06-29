9952b77a6a396b06c0eb5127bc5f19554aad6bae
Hung-Alex <alexanderpham59@gmai.com>
2024-06-29 14:16:59 +0700

    remove subcategories, adjust categories

--- src/backend/Domain/Entities/Category/Categories.cs
+++ src/backend/Domain/Entities/Category/Categories.cs
@@ -8,20 +8,23 @@ namespace Domain.Entities.Category
     public class Categories : BaseEntity, IDatedModification, IAggregateRoot
     {
         public Categories() { }
-        private Categories(string name, string descripton, string urlSlug, string image) : base()
+        private Categories(string name, string descripton, string urlSlug, string image, Guid? parrentId) : base()
         {
             Name = name ?? throw new ArgumentNullException();
             Description = descripton ?? throw new ArgumentNullException();
             UrlSlug = urlSlug ?? throw new ArgumentNullException();
             Image = image ?? throw new ArgumentNullException();
+            ParrentId = parrentId;
         }
         public string Name { get; set; }
+        public Categories ParentCategories { get; set; }
+        public Guid? ParrentId { get; set; }
         public string UrlSlug { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }//base 64
-        public ICollection<SubCategory> SubCategories { get; set; }
+        public ICollection<Categories> SubCategories { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-        
+
     }
 }
