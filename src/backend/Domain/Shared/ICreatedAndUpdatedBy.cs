e9bac149503f41c279e138b26bf33462de2f5219
Hung-Alex <alexanderpham59@gmai.com>
2024-07-05 14:15:05 +0700

    change fields CreatedByUserId and  UpdatedByUserId into able null

--- src/backend/Domain/Shared/ICreatedAndUpdatedBy.cs
+++ src/backend/Domain/Shared/ICreatedAndUpdatedBy.cs
@@ -4,9 +4,9 @@ namespace Domain.Shared
 {
     public interface ICreatedAndUpdatedBy
     {
-        public Guid CreatedByUserId { get; set; }
+        public Guid? CreatedByUserId { get; set; }
         public User CreatedByUser { get; set; }
-        public Guid UpdatedByUserId { get; set; }
+        public Guid? UpdatedByUserId { get; set; }
         public User UpdatedByUser { get; set; }
     }
 }
