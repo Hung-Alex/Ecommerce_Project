8f9b12ca00ea9013f17a054794d07277cc29376f
Hung-Alex <alexanderpham59@gmai.com>
2024-07-10 01:57:51 +0700

    add brand entity

---
+++ src/backend/Domain/Entities/Brands/Brand.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+using Domain.Entities.Products;
+using Domain.Entities.Users;
+using Domain.Shared;
+
+namespace Domain.Entities.Brands
+{
+    public class Brand : BaseEntity, IDatedModification, ICreatedAndUpdatedBy
+    {
+        public Brand() { }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public Guid? CreatedByUserId { get; set; }
+        public User CreatedByUser { get; set; }
+        public Guid? UpdatedByUserId { get; set; }
+        public User UpdatedByUser { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
