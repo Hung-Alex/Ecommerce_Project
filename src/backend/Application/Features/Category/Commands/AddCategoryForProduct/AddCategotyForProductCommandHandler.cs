b567e7f8bcccebda592e686f819739ec6a1461dd
Hung-Alex <alexanderpham59@gmai.com>
2024-07-09 22:06:16 +0700

    set category for product

---
+++ src/backend/Application/Features/Category/Commands/AddCategoryForProduct/AddCategotyForProductCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interface;
+using Domain.Entities;
+using Domain.Shared;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Features.Category.Commands.AddCategoryForProduct
+{
+    public sealed class AddCategotyForProductCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<AddCategotyForProductCommand, Result<bool>>
+    {
+        internal class AddCategotyForProductCommandValidator : AbstractValidator<AddCategotyForProductCommand>
+        {
+            public AddCategotyForProductCommandValidator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public async Task<Result<bool>> Handle(AddCategotyForProductCommand request, CancellationToken cancellationToken)
+        {
+            var repo = unitOfWork.GetRepository<ProductSubCategory>();
+            repo.Add(new ProductSubCategory { ProductId = request.ProductId, CategoryId = request.ParrentCategoryId });
+            if (request.SubCategoryId is not null)
+            {
+                repo.Add(new ProductSubCategory { ProductId = request.ProductId, CategoryId = (Guid)request.SubCategoryId });
+            }
+            await unitOfWork.Commit();
+            return Result<bool>.ResultSuccess(true);
+        }
+    }
+}
--- src/backend/WebMemoryzoneApi/Controllers/CategoryController.cs
+++ src/backend/WebMemoryzoneApi/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.DTOs.Filters.Categories;
+using Application.Features.Category.Commands.AddCategoryForProduct;
 using Application.Features.Category.Commands.CreateCategory;
 using Application.Features.Category.Commands.DeleteCategory;
 using Application.Features.Category.Commands.UpdateCategory;
@@ -63,6 +64,13 @@ namespace WebMemoryzoneApi.Controllers
         [HttpPost]
         [FileValidatorFilter<CreateCategoryCommand>([".png", ".jpg"], 1024 * 1024)]
         public async Task<IActionResult> AddCategory([FromForm] CreateCategoryCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (!result.IsSuccess) return BadRequest(result);
+            return Ok();
+        }
+        [HttpPost("addcategoryforproduct")]
+        public async Task<IActionResult> AddCategoryForProduct([FromBody] AddCategotyForProductCommand command)
         {
             var result = await _mediator.Send(command);
             if (!result.IsSuccess) return BadRequest(result);
