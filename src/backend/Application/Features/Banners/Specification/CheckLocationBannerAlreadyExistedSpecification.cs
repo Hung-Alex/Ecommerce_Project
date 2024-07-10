c0aef7c890fffd184f6c72cf444ed3ad92978dad
Hung-Alex <alexanderpham59@gmai.com>
2024-07-10 23:26:14 +0700

    create condition to check location already existed

---
+++ src/backend/Application/Features/Banners/Specification/CheckLocationBannerAlreadyExistedSpecification.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.Banners;
+using Domain.Specifications;
+using System.Linq.Expressions;
+using static Domain.Enums.BannerEnum;
+
+namespace Application.Features.Banners.Specification
+{
+    public class CheckLocationBannerAlreadyExistedSpecification : BaseSpecification<Banner>
+    {
+        private readonly LocationBanner? _locationBanner;
+        private readonly Guid? _id;
+        public CheckLocationBannerAlreadyExistedSpecification(LocationBanner? locationBanner, Guid? id)
+        {
+            _id = id;
+            _locationBanner = locationBanner;
+        }
+        public override Expression<Func<Banner, bool>> Criteria => b => b.Location == _locationBanner && b.Id != _id;
+    }
+}
