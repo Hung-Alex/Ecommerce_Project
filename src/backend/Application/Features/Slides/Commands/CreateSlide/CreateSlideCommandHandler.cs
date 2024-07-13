3582bed8fbfa3dc947a734a4ece826bc9add9b87
Hung-Alex <alexanderpham59@gmai.com>
2024-07-13 21:19:39 +0700

    refactor slide

--- src/backend/Application/Features/Slides/Commands/CreateSlide/CreateSlideCommandHandler.cs
+++ src/backend/Application/Features/Slides/Commands/CreateSlide/CreateSlideCommandHandler.cs
@@ -1,4 +1,7 @@
 ﻿using Application.Common.Interface;
+using Domain.Constants;
+using Domain.Entities.Images;
+using Domain.Entities.Products;
 using Domain.Entities.Slides;
 using Domain.Shared;
 using FluentValidation;
@@ -7,22 +10,55 @@ using MediatR;
 
 namespace Application.Features.Slides.Commands.CreateSlide
 {
-    public class CreateSlideCommandHandler : IRequestHandler<CreateSlideCommand, Result<bool>>
+    public sealed class CreateSlideCommandHandler : IRequestHandler<CreateSlideCommand, Result<bool>>
     {
-        internal class CreateBrandCommandValidator : AbstractValidator<CreateBrandCommandValidator>
+        internal class CreateSlideCommandValidator : AbstractValidator<CreateSlideCommand>
         {
 
         }
         private readonly IUnitOfWork _unitOfWork;
 
-        public CreateSlideCommandHandler(IUnitOfWork unitOfWork)
+        private readonly IMedia _media;
+        public CreateSlideCommandHandler(IUnitOfWork unitOfWork, IMedia media)
         {
             _unitOfWork = unitOfWork;
+            _media = media;
+            _media = media;
         }
         public async Task<Result<bool>> Handle(CreateSlideCommand request, CancellationToken cancellationToken)
         {
             var repoSlide = _unitOfWork.GetRepository<Slide>();
-            repoSlide.Add(new Slide(request.Title, request.Description, request.Status, request.Order));
+            var repoImage = _unitOfWork.GetRepository<Image>();
+            var slide = new Slide(request.Title, request.Description, request.Status, request.Order);
+            #region hanle Images
+            var image = new Image();
+            if (request.Images is not null)
+            {
+                int Count = 0;
+                foreach (var item in request.Images)
+                {
+                    var uploadResult = await _media.UploadLoadImageAsync(item, UploadFolderConstants.FolderProduct);
+                    if (uploadResult.IsSuccess)
+                    {
+                        image = new Image()
+                        {
+                            ImageExtension = item.ContentType
+                        ,
+                            ImageUrl = uploadResult.Data.Url
+                        ,
+                            PublicId = uploadResult.Data.PublicId
+                        };
+                        repoImage.Add(image);
+                        slide.SlidesImages.Add(new SlidesImage() { ImageId = image.Id, SlideId = slide.Id, OrderItem = Count++ });
+                    }
+                    else
+                    {
+                        return Result<bool>.ResultFailures(ErrorConstants.UploadImageOccursErrorWithFileName(item.FileName));
+                    }
+                }
+            }
+            #endregion
+            repoSlide.Add(slide);
             await _unitOfWork.Commit();
             return Result<bool>.ResultSuccess(true);
         }
