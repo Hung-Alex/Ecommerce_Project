2064bd558bd01e48f2306a59c9bde539e9e5784c
Hung-Alex <alexanderpham59@gmai.com>
2024-07-18 21:32:23 +0700

    refactor specification

---
+++ src/backend/Application/Features/Slides/Specification/GetSlideIsActiveSpecification.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities.Slides;
+using Domain.Specifications;
+using System.Linq.Expressions;
+
+namespace Application.Features.Slides.Specification
+{
+    public class GetSlideIsActiveSpecification : BaseSpecification<Slide>
+    {
+        public GetSlideIsActiveSpecification()
+        {
+            AddInclude(s => s.SlidesImages);
+        }
+        public override Expression<Func<Slide, bool>> Criteria => s => s.IsActive == true;
+    }
+}
