7cad36ab8bf755f9f977c5d720d7db03d8f98e1e
Hung-Alex <alexanderpham59@gmai.com>
2024-07-18 21:31:34 +0700

    get slide is active

---
+++ src/backend/Application/Features/Slides/Queries/GetSlideActive/GetSlideIsActiveQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.DTOs.Responses.Slides;
+using Domain.Shared;
+using MediatR;
+namespace Application.Features.Slides.Queries.GetSlideActive
+{
+    public record GetSlideIsActiveQuery:IRequest<Result<SlideDTO>>;
+}
---
+++ src/backend/Application/Features/Slides/Queries/GetSlideActive/GetSlideIsActiveQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interface;
+using Application.DTOs.Responses.Slides;
+using Application.Features.Slides.Specification;
+using Domain.Entities.Slides;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Features.Slides.Queries.GetSlideActive
+{
+    public sealed class GetSlideIsActiveQueryHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetSlideIsActiveQuery, Result<SlideDTO>>
+    {
+        public async Task<Result<SlideDTO>> Handle(GetSlideIsActiveQuery request, CancellationToken cancellationToken)
+        {
+
+            var repo = unitOfWork.GetRepository<Slide>();
+            var specification = new GetSlideIsActiveSpecification();
+            var slide = await repo.FindOneAsync(specification);
+            return Result<SlideDTO>.ResultSuccess(
+                new SlideDTO
+                {
+                    Id = slide.Id
+                    ,
+                    Title = slide.Title
+                    ,
+                    Description = slide.Description
+                    ,
+                    Images = slide.SlidesImages.Select(x => x.ImageUrl).ToList()
+                });
+        }
+    }
+}
