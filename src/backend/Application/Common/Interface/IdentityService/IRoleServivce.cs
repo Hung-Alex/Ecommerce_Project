eec2da3c02a3a3653f81d42bfec58886758a9c8d
Hung-Alex <alexanderpham59@gmai.com>
2024-07-20 16:39:15 +0700

    role_permission service

--- src/backend/Application/Common/Interface/IdentityService/IRoleServivce.cs
+++ src/backend/Application/Common/Interface/IdentityService/IRoleServivce.cs
@@ -1,8 +1,15 @@
-﻿
+﻿using Application.DTOs.Internal.Role;
+using Domain.Shared;
+
 namespace Application.Common.Interface.IdentityService
 {
     public interface IRoleServivce
     {
-        Task<bool> AssignmentRoleForUserAsync(Guid userId, string role, CancellationToken cancellationToken = default);
+        Task<Result<bool>> AssignmentRoleForUserAsync(Guid userId, string role, CancellationToken cancellationToken = default);
+        Task<Result<bool>> DeleteRoleFromUserAsync(Guid userId, Guid roleId, CancellationToken cancellationToken = default);
+        Task<Result<bool>> CreateRoleAsync(string name, CancellationToken cancellationToken = default);
+        Task<Result<bool>> UpdateRoleAsync(Guid roleId,string name, CancellationToken cancellationToken = default);
+        Task<bool> IsInRoleAsync(Guid userId, Guid roleId, CancellationToken cancellationToken = default);
+        Task<RoleInternal> GetRoleAsync(Guid roleId, CancellationToken cancellationToken = default);
     }
 }
---
+++ src/backend/Infrastructure/Services/Auth/PermissionAuthorizationHandler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Infrastructure.Services.Auth
+{
+    public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
