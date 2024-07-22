a041a412bbeea90abb2a530b4dc85fbbdf1e9668
Hung-Alex <alexanderpham59@gmai.com>
2024-07-22 21:11:49 +0700

    define application permission

---
+++ src/backend/Domain/Enums/PermissionEnum.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Enums
+{
+    public static class PermissionEnum
+    {
+        public enum Permission
+        {
+            CreateProduct,
+            UpdateProduct,
+            DeleteProduct,
+            DeleteImage,
+            CreateBrand,
+            UpdateBrand,
+            DeleteBrand,
+            CreateBanner,
+            UpdateBanner,
+            DeleteBanner,
+            CreatePost,
+            UpdatePost,
+            DeletePost,
+            CreateSlide,
+            UpdateSlide,
+            DeleteSlide,
+            CreateCategory,
+            UpdateCategory,
+            DeleteCategory,
+        }
+    }
+}
