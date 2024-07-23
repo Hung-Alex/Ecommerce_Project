07a8a054ccdbddd7ae5c4eef88aff7be4657878d
Hung-Alex <alexanderpham59@gmai.com>
2024-07-23 17:26:52 +0700

    delete images

---
+++ src/backend/Application/Features/Images/Command/DeleteImageCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interface;
+using Domain.Constants;
+using Domain.Entities;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Features.Images.Command
+{
+    public sealed class DeleteImageCommandHandler(IUnitOfWork unitOfWork, IMedia media) : IRequestHandler<DeleteImageCommand, Result<bool>>
+    {
+        public async Task<Result<bool>> Handle(DeleteImageCommand request, CancellationToken cancellationToken)
+        {
+            var repo = unitOfWork.GetRepository<Image>();
+            var image = await repo.GetByIdAsync(request.id);
+            if (image == null) { return Result<bool>.ResultFailures(ErrorConstants.NotFoundWithId(request.id)); }
+            var isDelete = await media.DeleteImageAsync(image.PublicId);
+            if (isDelete.IsSuccess is false)
+            {
+                return Result<bool>.ResultFailures(isDelete.Errors);
+            }
+            return Result<bool>.ResultSuccess(true);
+        }
+    }
+}
---
+++ src/backend/WebMemoryzoneApi/Controllers/ImageController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Features.Images.Command;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMemoryzoneApi.Controllers
+{
+    [ApiController]
+    [Route("api/images")]
+    public class ImageController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ImageController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpDelete("{id:Guid}")]
+        public async Task<ActionResult> DeleteImage(Guid id)
+        {
+            var result = await _mediator.Send(new DeleteImageCommand(id));
+            if (result.IsSuccess is false)
+            {
+                return NotFound(result);
+            }
+            return Ok(result);
+        }
+    }
+}
