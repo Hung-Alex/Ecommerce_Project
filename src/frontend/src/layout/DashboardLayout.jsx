d391c904d90464a67ee749a4fcc6869b237a337b
Hồ Trương Huệ Nhật <126588311+hotruonghuenhat@users.noreply.github.com>
2024-07-23 20:57:31 +0700

    fix sidebar

--- src/frontend/src/layout/DashboardLayout.jsx
+++ src/frontend/src/layout/DashboardLayout.jsx
@@ -6,7 +6,7 @@ const DashboardLayout = ({ children }) => {
   return (
     <>
       <AdminNavbar />
-    <div className="flex justify-center items-start bg-slate-100">
+    <div className="flex h-svh justify-center items-start bg-slate-100">
       <SideNav />
       {children}
     </div>
