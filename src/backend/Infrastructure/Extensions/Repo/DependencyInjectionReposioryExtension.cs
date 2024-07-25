15a6479f159d1d526947d5d66dbb582d715333dd
Hung-Alex <alexanderpham59@gmai.com>
2024-07-25 23:32:21 +0700

    refactor  structure dependencyInjection

---
+++ src/backend/Infrastructure/Extensions/Repo/DependencyInjectionReposioryExtension.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Interface;
+using Domain.Interface;
+using Infrastructure.Repositories.GenericRepository;
+using Infrastructure.Repositories.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions.Repo
+{
+    internal static class DependencyInjectionReposioryExtension
+    {
+        internal static IServiceCollection AddRepositoryExtension(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            return services;
+        }
+    }
+}
