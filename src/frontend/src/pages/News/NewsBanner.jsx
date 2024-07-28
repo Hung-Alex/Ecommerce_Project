71ca4e6921f1100cf98351e306089e25422bb576
Hồ Trương Huệ Nhật <126588311+hotruonghuenhat@users.noreply.github.com>
2024-07-28 23:58:45 +0700

    call api News page

--- src/frontend/src/pages/News/NewsBanner.jsx
+++ src/frontend/src/pages/News/NewsBanner.jsx
@@ -1,70 +1,39 @@
 import React from 'react';
+import { formatDate } from '../../utils/formatDate';
 
-const newsData = [
-    {
-        id: 1,
-        category: "Art",
-        date: "31 Jul",
-        title: "Fuga ea ullam earum assumenda, beatae labore eligendi.",
-        imageUrl: "https://source.unsplash.com/random/245x320"
-    },
-    {
-        id: 2,
-        category: "Politics",
-        date: "04 Aug",
-        title: "Autem sunt tempora mollitia magnam non voluptates",
-        imageUrl: "https://source.unsplash.com/random/240x320"
-    },
-    {
-        id: 3,
-        category: "Health",
-        date: "01 Aug",
-        title: "Inventore reiciendis aliquam excepturi",
-        imageUrl: "https://source.unsplash.com/random/241x320"
-    },
-    {
-        id: 4,
-        category: "Science",
-        date: "28 Jul",
-        title: "Officiis mollitia dignissimos commodi optio vero animi",
-        imageUrl: "https://source.unsplash.com/random/242x320"
-    },
-    {
-        id: 5,
-        category: "Sports",
-        date: "19 Jul",
-        title: "Doloribus sit illo necessitatibus architecto exercitationem enim",
-        imageUrl: "https://source.unsplash.com/random/243x320"
-    },
-];
+const NewsBanner = ({ newsData }) => {
+  const displayedNews = newsData.slice(0, 5);
 
-const NewsBanner = () => {
-    return (
-        <div className="max-w-screen-xl p-5 mx-auto bg-white">
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 md:gap-0 lg:grid-rows-2">
-                {newsData.map((news, index) => (
-                    <div
-                        key={news.id}
-                        className={`relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group
-                            ${index === 0 ? 'md:col-span-2 lg:row-span-2 lg:h-full' : 'hidden lg:block'}`}
-                        style={{ backgroundImage: `url(${news.imageUrl})` }}
-                    >
-                        <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-gray-100"></div>
-                        <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
-                            <a href="#" className="px-3 py-2 text-xs font-semibold tracking-wider uppercase hover:underline text-gray-800 bg-violet-600">{news.category}</a>
-                            <div className="flex flex-col justify-start text-center text-gray-800">
-                                <span className="text-3xl font-semibold leading-none tracking-wide">{news.date.split(' ')[0]}</span>
-                                <span className="leading-none uppercase">{news.date.split(' ')[1]}</span>
-                            </div>
-                        </div>
-                        <h2 className="z-10 p-5">
-                            <a href="#" className="font-medium text-md group-hover:underline text-gray-800">{news.title}</a>
-                        </h2>
-                    </div>
-                ))}
+  return (
+    <div className="max-w-screen-xl p-5 mx-auto bg-white">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 md:gap-0 lg:grid-rows-2">
+        {displayedNews.map((news, index) => (
+          <a
+            href={`/news/${news.urlSlug}`}
+            key={news.id}
+            className={`relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group
+              ${index === 0 ? 'md:col-span-2 lg:row-span-2 lg:h-full' : 'hidden lg:block'}`}
+            style={{ backgroundImage: `url(${news.image})` }}
+          >
+            <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-black-100 transition-opacity duration-300 group-hover:opacity-70"></div>
+            <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
+              <a href={`/news/${news.urlSlug}`} className="px-3 py-2 text-xs font-semibold tracking-wider uppercase hover:underline text-gray-800 bg-violet-600">
+                {formatDate(news.createdAt)}
+              </a>
+              <div className="flex flex-col justify-start text-center text-gray-800">
+                <span className="text-3xl font-semibold leading-none tracking-wide"></span>
+                <span className="leading-none uppercase"></span>
+              </div>
             </div>
-        </div>
-    );
+            <h2 className="z-10 pl-5 py-5 pt-12">
+              <a href={`/news/${news.urlSlug}`} className="font-medium text-lg group-hover:underline text-white">{news.title}</a>
+            </h2>
+            <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 group-hover:opacity-30"></div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default NewsBanner;
--- src/frontend/src/pages/News/NewsSection.jsx
+++ src/frontend/src/pages/News/NewsSection.jsx
@@ -1,74 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { formatDate } from '../../utils/formatDate';
 
-const NewsSection = () => {
-  const newsPosts = [
-    {
-      title: "How to use sticky note for problem solving",
-      date: "20 October 2019",
-      image: "https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    },
-    {
-      title: "How to use sticky note for problem solving",
-      date: "20 October 2019",
-      image: "https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    },
-    {
-      title: "Morning routine to boost your mood",
-      date: "25 November 2020",
-      image: "https://images.unsplash.com/photo-1544654803-b69140b285a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    },
-    {
-      title: "All the features you want to know",
-      date: "30 September 2020",
-      image: "https://images.unsplash.com/photo-1530099486328-e021101a494a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1547&q=80",
-    },
-    {
-      title: "Minimal workspace for your inspirations",
-      date: "13 October 2019",
-      image: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1484&q=80",
-    },
-    {
-      title: "What do you want to know about Blockchain",
-      date: "20 October 2019",
-      image: "https://images.unsplash.com/photo-1624996379697-f01d168b1a52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    },
-  ];
+const NewsSection = ({newsData}) => {
+  // const newsPosts = [
+  //   {
+  //     title: "How to use sticky note for problem solving",
+  //     date: "20 October 2019",
+  //     image: "https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  //   },
+  //   {
+  //     title: "How to use sticky note for problem solving",
+  //     date: "20 October 2019",
+  //     image: "https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  //   },
+  //   {
+  //     title: "Morning routine to boost your mood",
+  //     date: "25 November 2020",
+  //     image: "https://images.unsplash.com/photo-1544654803-b69140b285a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  //   },
+  //   {
+  //     title: "All the features you want to know",
+  //     date: "30 September 2020",
+  //     image: "https://images.unsplash.com/photo-1530099486328-e021101a494a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1547&q=80",
+  //   },
+  //   {
+  //     title: "Minimal workspace for your inspirations",
+  //     date: "13 October 2019",
+  //     image: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1484&q=80",
+  //   },
+  //   {
+  //     title: "What do you want to know about Blockchain",
+  //     date: "20 October 2019",
+  //     image: "https://images.unsplash.com/photo-1624996379697-f01d168b1a52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  //   },
+  // ];
 
   // Utility function to format the title for the URL
   const formatTitleForURL = (title) => {
     return title.toLowerCase().replace(/\s+/g, '-');
   };
 
+
   return (
     <section className="bg-white">
-      <div className="container px-6 py-10 mx-auto">
-        <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl">From the news</h1>
-        <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
-          {newsPosts.map((post, index) => (
-            <Link
-              key={index}
-              to={`/news/${formatTitleForURL(post.title)}`}
-              className="2xl:flex block hover:bg-gray-100 rounded-lg transition-colors duration-300"
-            >
-              <div className="overflow-hidden rounded-lg 2xl:w-64">
-                <img
-                  className="object-cover w-full h-56 transform transition-transform duration-300 ease-in-out hover:scale-105"
-                  src={post.image}
-                  alt={post.title}
-                />
-              </div>
-              <div className="flex flex-col justify-between py-6 2xl:mx-6">
-                <span className="text-xl font-semibold text-gray-800 hover:underline">
-                  {post.title}
-                </span>
-                <span className="text-sm text-gray-500">On: {post.date}</span>
-              </div>
-            </Link>
-          ))}
-        </div>
+    <div className="container px-6 py-10 mx-auto">
+      <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl">From the news</h1>
+      <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-3">
+        {newsData.map((post, index) => (
+          <Link
+            key={index}
+            to={`/news/${post.urlSlug}`}
+            className="block hover:bg-gray-100 rounded-lg transition-colors duration-300"
+          >
+            <div className="overflow-hidden rounded-lg">
+              <img
+                className="object-cover w-full h-48 md:h-64 lg:h-72 xl:h-80 2xl:h-96 transition-transform duration-300 ease-in-out hover:scale-105"
+                src={post.image}
+                alt={post.title}
+              />
+            </div>
+            <div className="flex flex-col justify-between py-6 mx-6">
+              <span className="text-xl font-semibold text-gray-800 hover:underline">
+                {post.title}
+              </span>
+              <span className="text-md text-gray-600 hover:underline hidden md:block">
+                {post.shortDescription}
+              </span>
+              <span className="text-sm text-gray-500">on: {formatDate(post.createdAt)}</span>
+            </div>
+          </Link>
+        ))}
       </div>
-    </section>
+    </div>
+  </section>
   );
 };
 
